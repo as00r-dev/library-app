414f38a4eb03589223c9bb4085c613aa5368077b
as00r <as00r.dev@gmail.com>
2022-11-27 19:29:00 +0530

    Add render book function and filters

--- script.js
+++ script.js
@@ -41,10 +41,64 @@ function addBookToArray(e) {
 	const inputBook = new Book(
 		name.value,
 		author.value,
-		totalPages.value,
+		Number(totalPages.value),
 		imageUrl.value,
-		pagesRead.value
+		Number(pagesRead.value)
 	);
 	books.push(inputBook);
+	clearScreen();
+	renderBooks();
 	closeModal();
+}
+
+// Render Books from books array
+
+const bookshelf = document.querySelector(".bookshelf");
+const filterRead = document.querySelector(".filter-option.read .read-books");
+const filterReading = document.querySelector(
+	".filter-option.reading .reading-books"
+);
+const filterToRead = document.querySelector(
+	".filter-option.to-read .to-read-books"
+);
+function renderBooks() {
+	let reading = 0;
+	let finished = 0;
+	let notStarted = 0;
+	books.forEach((book) => {
+		const bookWrapper = document.createElement("div");
+		bookWrapper.classList.add("book");
+		bookWrapper.style.setProperty("--image", `url(${book.imageUrl})`);
+		const bookName = document.createElement("div");
+		bookName.classList.add("book-name");
+		bookName.textContent = "Name: " + book.name;
+		const authorName = document.createElement("div");
+		authorName.classList.add("author");
+		authorName.textContent = "Author: " + book.author;
+		const readPages = document.createElement("div");
+		readPages.classList.add("read-pages");
+		readPages.textContent = "Pages Read: " + book.pagesRead;
+		const totalPages = document.createElement("div");
+		totalPages.classList.add("total-pages");
+		totalPages.textContent = "Total Pages: " + book.totalPages;
+		bookWrapper.append(bookName, authorName, readPages, totalPages);
+		bookshelf.appendChild(bookWrapper);
+		if (book.pagesRead === 0) {
+			notStarted++;
+		} else if (book.pagesRead < book.totalPages && book.pagesRead > 0) {
+			reading++;
+		} else if (book.totalPages === book.pagesRead) {
+			finished++;
+		}
+		filterRead.innerHTML = "";
+		filterRead.textContent = finished;
+		filterToRead.innerHTML = "";
+		filterToRead.textContent = notStarted;
+		filterReading.innerHTML = "";
+		filterReading.textContent = reading;
+	});
+}
+
+function clearScreen() {
+	bookshelf.innerHTML = "";
 }
