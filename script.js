894292dc0a46d38ba40d32dae0b3e270ba6137fd
as00r <as00r.dev@gmail.com>
2022-11-27 20:48:53 +0530

    Add sample books

--- script.js
+++ script.js
@@ -17,7 +17,106 @@ function closeModal() {
 
 // Add book
 
-const books = [];
+const books = [
+	new Book(
+		"Crime and Punishment",
+		"Dostoevsky",
+		485,
+		"https://m.media-amazon.com/images/I/A193gO2P8WL.jpg",
+		485
+	),
+	new Book(
+		"The Brothers Karamazov",
+		"Dostoevsky",
+		880,
+		"https://m.media-amazon.com/images/I/71OZJsgZzQL.jpg",
+		644
+	),
+	new Book(
+		"Raat Pashmine Ki",
+		"Gulzaar",
+		200,
+		"https://m.media-amazon.com/images/I/41fyzq32nfL.jpg",
+		13
+	),
+	new Book(
+		"The Iliad",
+		"Homer",
+		352,
+		"https://m.media-amazon.com/images/I/71CdBsMx42L.jpg",
+		158
+	),
+	new Book(
+		"Tomb of Sand",
+		"Dostoevsky",
+		716,
+		"https://m.media-amazon.com/images/I/81vHR1cX5eL.jpg",
+		0
+	),
+	new Book(
+		"The Story Of Philosophy",
+		"Will Durant",
+		700,
+		"https://m.media-amazon.com/images/I/71SHr0oIiyL.jpg",
+		157
+	),
+	new Book(
+		"The Art Of War",
+		"Sun Tzu",
+		103,
+		"https://m.media-amazon.com/images/I/71Ow6MUOx2L.jpg",
+		103
+	),
+	new Book(
+		"Meditations",
+		"Marcus Aurelius",
+		149,
+		"https://m.media-amazon.com/images/I/817HOFZewXL.jpg",
+		22
+	),
+	new Book(
+		"Thus Spoke Zarathustra",
+		"Nietzsche",
+		332,
+		"https://m.media-amazon.com/images/I/9176A9Hpd2L.jpg",
+		25
+	),
+	new Book(
+		"Chanakya Neeti",
+		"Chanakya",
+		152,
+		"https://m.media-amazon.com/images/I/81NFENSYUwL.jpg",
+		0
+	),
+	new Book(
+		"How to Win Friends & Influence People",
+		"Dale Carnegie",
+		364,
+		"https://m.media-amazon.com/images/I/611OWa8x+WL.jpg",
+		364
+	),
+	new Book(
+		"The Art of Thinking Clearly",
+		"Rof Dobelli",
+		326,
+		"https://m.media-amazon.com/images/I/41IjIRNIH7L.jpg",
+		326
+	),
+	new Book(
+		"How To Read a Book",
+		"Mortimer J Adler, Charles Van Doren",
+		424,
+		"https://m.media-amazon.com/images/I/51EG8AyJiQL._SX320_BO1,204,203,200_.jpg",
+		424
+	),
+	new Book(
+		"Freakonomics",
+		"Steven D Levitt, Stephen J Dubner",
+		315,
+		"https://m.media-amazon.com/images/I/51gYI2oq6KL.jpg",
+		315
+	),
+];
 
 function Book(name, author, totalPages, imageUrl, pagesRead) {
 	this.name = name;
@@ -38,14 +137,16 @@ function addBookToArray(e) {
 	const totalPages = document.querySelector("form .total-pages");
 	const pagesRead = document.querySelector("form .pages-read");
 	const imageUrl = document.querySelector("form .image-url");
-	const inputBook = new Book(
-		name.value,
-		author.value,
-		Number(totalPages.value),
-		imageUrl.value,
-		Number(pagesRead.value)
-	);
-	books.push(inputBook);
+	if (name.value && author.value && totalPages && pagesRead && imageUrl) {
+		const inputBook = new Book(
+			name.value,
+			author.value,
+			Number(totalPages.value),
+			imageUrl.value,
+			Number(pagesRead.value)
+		);
+		books.push(inputBook);
+	}
 	clearScreen();
 	renderBooks();
 	closeModal();
@@ -61,7 +162,9 @@ const filterReading = document.querySelector(
 const filterToRead = document.querySelector(
 	".filter-option.to-read .to-read-books"
 );
+const filterAll = document.querySelector(".filter-option.all .all-books");
 function renderBooks() {
+	let all = books.length;
 	let reading = 0;
 	let finished = 0;
 	let notStarted = 0;
@@ -96,9 +199,13 @@ function renderBooks() {
 		filterToRead.textContent = notStarted;
 		filterReading.innerHTML = "";
 		filterReading.textContent = reading;
+		filterAll.innerHTML = "";
+		filterAll.textContent = all;
 	});
 }
 
 function clearScreen() {
 	bookshelf.innerHTML = "";
 }
+
+renderBooks();
