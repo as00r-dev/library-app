2dddbb6766dc32ef406d1b92b193add75d3c3153
as00r <as00r.dev@gmail.com>
2022-11-30 03:18:12 +0530

    Add a plan for code rehaul

---
+++ development.js
@@ -0,0 +1,21 @@
+// Make a module for interacting with books
+// --- Creates a new Book
+// --- Shows Book
+// --- Has the books array
+
+// Make a module for interacting with DOM
+// --- Has event listeners
+// --- Has all the DOM elements
+// --- Updates the DOM with new Book
+
+// Use the Module Pattern and Composition design pattern.
+// Bonus things to remember and use
+// --- Closures
+// --- IIFE
+// --- Prototypes and Prototypal Inheritance
+// --- Private Scope and Factory Functions
+
+// Goal
+// --- Implement functional programming in a real project
+// --- Write better code
+// --- Implement breaking of logic into smaller pieces of logic
